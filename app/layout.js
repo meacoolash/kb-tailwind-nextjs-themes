e69826316d0ad3e3f50f5eea2ef63b40655a12e0
Miki Stec <mikulas@stec.sk>
2024-01-04 21:25:43 +0100

    themese implementation

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Providers } from './provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,8 +11,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html suppressHydrationWarning lang="en">
+      <body className={`${inter.variable}`}>
+        <Providers>
+          {children}
+          <div className='text-secondary'>asdf</div>
+        </Providers>
+      </body>
     </html>
   )
 }
